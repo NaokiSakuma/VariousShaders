a1fe65548e4b17f2368f6b74a5329bdabae303f7
NaokiSakuma <soramamenatan@gmail.com>
2023-01-22 15:05:13 +0900

    refactor switcher

--- Assets/Utility/Scripts/RenderPipelineSwitcher.cs
+++ Assets/Utility/Scripts/RenderPipelineSwitcher.cs
@@ -3,12 +3,23 @@ using UnityEngine.Rendering;
 
 namespace Utility
 {
+    [ExecuteAlways]
     public class RenderPipelineSwitcher : MonoBehaviour
     {
         [SerializeField]
         private RenderPipelineAsset renderPipelineAsset;
 
+        private void Awake()
+        {
+            ChangeRenderPipeline();
+        }
+
         private void OnValidate()
+        {
+            ChangeRenderPipeline();
+        }
+
+        private void ChangeRenderPipeline()
         {
             if (GraphicsSettings.renderPipelineAsset == renderPipelineAsset)
             {
